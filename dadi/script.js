86ddb2a203ce9f48df32a55ce32da0e52f051bf3
Nino Chimento <saninoc84@gmail.com>
2020-01-07 14:03:09 +0100

    creazione variabile numeri giocatori

--- dadi/script.js
+++ dadi/script.js
@@ -1,7 +1,8 @@
 document.getElementById("play").addEventListener("click",
 function () {
   for (var i = 1; i <= 7; i++) {
-    console.log(i);
+    numero1 = Math.floor(Math.random() * 6 + 1);
+    numero2 = Math.floor(Math.random() * 6 + 1);
     }
 }
 )
