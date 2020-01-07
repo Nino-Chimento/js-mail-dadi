ee7697f97f66eaa694878f3f1eb0d31d025f3f19
Nino Chimento <saninoc84@gmail.com>
2020-01-07 13:15:30 +0100

    creazione lista email e prompt email da inserire

---
+++ mail/script.js
@@ -0,0 +1,9 @@
+var emailInserita = prompt("inserisci la tua email")
+// for (var i = 0; i < emailInserita.length; i++) {
+//   var controllo = emailInserita[i]
+//
+//   if (controllo != "com") {
+//     console.log("nino");
+//   }
+// }
+var listaEmail = ["123.gmail.com","canechecorre@gmail.com","random@hotmail.it","saninoc84@gmail.com"]
