556d2e835acb7b30a6a618464777dd160ae927a3
Nino Chimento <saninoc84@gmail.com>
2020-01-07 15:14:37 +0100

    finish js

--- dadi/script.js
+++ dadi/script.js
@@ -1,23 +1,39 @@
-var PunteggioP1 = 0
-var PunteggioP2 = 0
+var punteggioP1 = 0
+var punteggioP2 = 0
 document.getElementById("play").addEventListener("click",
 function () {
   for (var i = 1; i <= 7; i++) {
     numero1 = Math.floor(Math.random() * 6 + 1);
     numero2 = Math.floor(Math.random() * 6 + 1);
-    document.getElementById('risultatoNumero1').innerHTML = numero1;
-    document.getElementById('risultatoNumero2').innerHTML = numero2;
+    document.getElementById('risultatoNumero1').innerHTML += numero1;
+    document.getElementById('risultatoNumero2').innerHTML += numero2;
     if (numero1 > numero2) {
-      alert("numero 1 maggiore");
-       PunteggioP1++
+      alert("Vince la mano il play1");
+       punteggioP1++
     }
     else if (numero1 < numero2) {
-      alert("numero 1 minore");
-      PunteggioP2++
+      alert("Vince la mano il play2");
+      punteggioP2++
+
     }
     else {
       alert("pareggio")
     }
+    document.getElementById('scoreFinale1').innerHTML = punteggioP1;
+    document.getElementById('scoreFinale2').innerHTML = punteggioP2;
   }
 }
 )
+// for(eccetera) {
+//   numero1 = Math.floor(Math.random() * 6 + 1);
+//   numero2 = Math.floor(Math.random() * 6 + 1);
+//   updateScore(numero1, numero2);
+// }
+// function updateScore(numero1, numero2) {
+// }
+// function updateScore(numero1, numero2) {
+//   setTimeout(function () {
+//   document.getElementById('risultatoNumero1').innerHTML += numero1;
+//   document.getElementById('risultatoNumero2').innerHTML += numero2;
+//  }, 1000);
+// }
