6536e873198b71cf935af3008b14c08d07d09c51
Nino Chimento <saninoc84@gmail.com>
2020-01-07 14:16:02 +0100

    silenziamento js funzionante con iterazione

--- dadi/script.js
+++ dadi/script.js
@@ -1,17 +1,18 @@
-document.getElementById("play").addEventListener("click",
-function () {
-  for (var i = 1; i <= 7; i++) {
-    numero1 = Math.floor(Math.random() * 6 + 1);
-    numero2 = Math.floor(Math.random() * 6 + 1);
-    if (numero1 > numero2) {
-      alert("numero 1 maggiore")
-    }
-    else if (numero1 < numero2) {
-      alert("numero 1 minore")
-    }
-    else {
-      alert("pareggio")
-    }
-  }
-}
-)
+// document.getElementById("play").addEventListener("click",
+// function () {
+//   for (var i = 1; i <= 7; i++) {
+//     numero1 = Math.floor(Math.random() * 6 + 1);
+//     numero2 = Math.floor(Math.random() * 6 + 1);
+//
+//     if (numero1 > numero2) {
+//       alert("numero 1 maggiore")
+//     }
+//     else if (numero1 < numero2) {
+//       alert("numero 1 minore")
+//     }
+//     else {
+//       alert("pareggio")
+//     }
+//   }
+// }
+// )
