c930583ac48bdfc67f8155d6e267eee8ce848475
Nino Chimento <saninoc84@gmail.com>
2020-01-07 13:23:36 +0100

    creazione var di verifica e messaggio in base alla verifica eseguita

--- mail/script.js
+++ mail/script.js
@@ -7,3 +7,18 @@ var emailInserita = prompt("inserisci la tua email")
 //   }
 // }
 var listaEmail = ["123.gmail.com","canechecorre@gmail.com","random@hotmail.it","saninoc84@gmail.com"]
+for (var i = 0; i < listaEmail.length; i++) {
+  listaEmail[i]
+  if (emailInserita == listaEmail[i]) {
+     emailVerificata = true;
+  }
+  else {
+    emailVerificata = false
+  }
+}
+if (emailVerificata == true) {
+  alert("benvenuto visitatore");
+}
+else {
+  alert("hai inserito un email non valida");
+}
